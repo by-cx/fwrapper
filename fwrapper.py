50b3cade0de7e3602bf53369d72b92b54aacc933
Adam Strauch <cx@initd.cz>
2012-05-17 02:12:23 +0200

    Fixes

--- fwrapper.py
+++ fwrapper.py
@@ -1,23 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # Imports
 
 import sys
 import datetime
 import shlex
-from subprocess import PIPE, POpen
+from subprocess import PIPE, Popen
 
 def log(msg):
     date = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
     print "%s: %s" % (date, msg)
 
 def run(cmd):
-    p = POpen()
+    if "debug" in sys.argv:
+        print " ".join(cmd)
+    if "off" in sys.argv:
+        return
+
+    p = Popen(cmd, stdout=PIPE, stderr=PIPE)
     stdout, stderr = p.communicate()
     if stdout:
-        print strout
+        log(strout)
     if stderr:
-        print strerr
+        log(strerr)
+        sys.exit(1)
 
 # Classes
 
@@ -26,6 +32,11 @@ class Firewall(object):
         "filter4": [],
         "filter6": [],
         "nat": [],
+    }
+    policy = {
+            "INPUT": "ACCEPT",
+            "OUTPUT": "ACCEPT",
+            "FORWARD": "ACCEPT",
     }
     filename = ""
     content = ""
@@ -45,27 +56,18 @@ class Firewall(object):
             if len(line) and line[0] != "#":
                 cmd = shlex.split(line)
                 if len(cmd):
-                    if cmd[0] == "filter":
+                    if cmd[0] in ["filter", "filter4", "filter64", "filter46"]:
                         self.rules["filter4"].append(cmd[1:])
-                    elif cmd[0] == "filter4":
-                        self.rules["filter4"].append(cmd[1:])
-                    elif cmd[0] == "filter6":
-                        self.rules["filter4"].append(cmd[1:])
-                    elif cmd[0] == "filter46":
-                        self.rules["filter4"].append(cmd[1:])
+                    if cmd[0] in ["filter6", "filter64", "filter46"]:
                         self.rules["filter6"].append(cmd[1:])
-                    elif cmd[0] == "filter64":
-                        self.rules["filter4"].append(cmd[1:])
-                        self.rules["filter6"].append(cmd[1:])
-                    elif cmd[0] == "nat":
+                    if cmd[0] == "nat":
                         self.rules["nat"].append(cmd[1:])
-                    else:
-                        log("Warning: wrong rule (%s)" % " ".join(cmd))
-    def stop_rules(self):
-        pass
+                    if cmd[0] in ["forward", "output", "input"]:
+                        self.policy[cmd[0].upper()] = cmd[1].upper()
 
     def rule_format(self, rule):
         chain = ""
+        prefix = ""
         action = ""
         options = []
         last = ""
@@ -75,8 +77,13 @@ class Firewall(object):
             elif last == "jump":
                 action = parm
 
-            if parm in ["-A", "-N"]:
+            if parm == "-A":
                 last = "chain"
+                prefix = "ADD to"
+                continue
+            if parm == "-N":
+                last = "chain"
+                prefix = "NEW   "
                 continue
             elif parm in ["-j"]:
                 last = "jump"
@@ -85,7 +92,59 @@ class Firewall(object):
                 options.append(parm)
 
             last = ""
-        return chain, " ".join(options), action
+        return "%s %s" % (prefix, chain), " ".join(options), action
+
+    def start_cmd_generator(self):
+        cmds = []
+
+        for ruletype in self.rules:
+            for rule in self.rules[ruletype]:
+                if ruletype in ["filter", "filter4", "filter64", "filter46"]:
+                    cmds.append(["iptables", "-t", "filter"] + rule)
+                if ruletype in ["filter6", "filter64", "filter46"]:
+                    cmds.append(["ip6tables", "-t", "filter"] + rule)
+                if ruletype == "nat":
+                    cmds.append(["iptables", "-t", "nat"] + rule)
+
+        for chain in self.policy:
+            cmds.append(["iptables", "-P", chain, self.policy[chain]])
+            cmds.append(["ip6tables", "-P", chain, self.policy[chain]])
+
+        return cmds
+
+    def stop_cmd_generator(self):
+        cmds = []
+
+        for ruletype in self.rules:
+            rules = self.rules[ruletype]
+            rules.reverse()
+            for rule in rules:
+                rule = map(lambda x: "-X" if "-N" == x else x, rule)
+                rule = map(lambda x: "-D" if "-A" == x else x, rule)
+                if ruletype in ["filter", "filter4", "filter64", "filter46"]:
+                    cmds.append(["iptables", "-t", "filter"] + rule)
+                if ruletype in ["filter6", "filter64", "filter46"]:
+                    cmds.append(["ip6tables", "-t", "filter"] + rule)
+                if ruletype == "nat":
+                    cmds.append(["iptables", "-t", "nat"] + rule)
+
+        for chain in self.policy:
+            cmds.append(["iptables", "-P", chain, "ACCEPT"])
+            cmds.append(["ip6tables", "-P", chain, "ACCEPT"])
+
+        return cmds
+
+    def start(self):
+        """Run commands to start firewall
+        """
+        cmds = self.start_cmd_generator()
+        for cmd in cmds:
+            run(cmd) 
+
+    def stop(self):
+        cmds = self.stop_cmd_generator()
+        for cmd in cmds:
+            run(cmd) 
 
     def print_rules(self):
         for ruletype in self.rules:
@@ -98,26 +157,30 @@ class Firewall(object):
             if ruletype == "nat":
                 print "Firewall - NAT"
                 print 
-            print "|",
-            print "Chain".ljust(20),
+            print " Chain".ljust(20),
             print "|",
             print "Options".ljust(55),
             print "|",
-            print "Action".ljust(20),
-            print "|"
+            print "Action".ljust(20)
+            print
+            print "-------------------------------------------------------------------------------------------------------"
+            print
 
             for rule in self.rules[ruletype]:
                 chain, options, action = self.rule_format(rule)
+                print chain.ljust(20),
+                print "|",
+                print options.ljust(55),
+                print "|",
+                print action.ljust(20)
 
             print 
-
 
 # Script load
 
 if len(sys.argv) < 2:
     log("No script to load")
     sys.exit(1)
-
 _script = sys.argv[1]
 
 firewall = Firewall(_script)
@@ -131,10 +194,10 @@ _action = sys.argv[2]
 # Functions
 
 def start():
-    pass
+    firewall.start()
 
 def stop():
-    pass
+    firewall.stop()
 
 def restart():
     stop()
@@ -144,13 +207,13 @@ def restart():
 
 if _action == "start":
     start()
-    log("Fw %s started")
+    log("Fw %s started" % _script)
 elif _action == "stop":
     stop()
-    log("Fw %s stopped")
+    log("Fw %s stopped" % _script)
 elif _action == "restart":
     restart()
-    log("Fw %s restarted")
+    log("Fw %s restarted" % _script)
 elif _action == "list":
     firewall.print_rules()
 else:
